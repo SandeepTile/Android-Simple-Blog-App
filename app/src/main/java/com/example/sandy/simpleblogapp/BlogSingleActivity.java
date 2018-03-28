0a2090ace0348644dfde12af9dce85c9c4e1511e
Sandeep <sandeeptile@gmail.com>
2018-03-28 21:03:46 +0530

    remove blog button is added

--- app/src/main/java/com/example/sandy/simpleblogapp/BlogSingleActivity.java
+++ app/src/main/java/com/example/sandy/simpleblogapp/BlogSingleActivity.java
@@ -1,11 +1,16 @@
 package com.example.sandy.simpleblogapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,9 +24,12 @@ public class BlogSingleActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
 
+    private FirebaseAuth mAuth;
+
     private ImageView mBlogSingleImage;
     private TextView mBlogSingleTitle;
     private TextView mBlogSingleDesc;
+    private Button mBlogSingleRemoveBtn;
 
 
     @Override
@@ -30,14 +38,15 @@ public class BlogSingleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_blog_single);
 
         mDatabase= FirebaseDatabase.getInstance().getReference().child("Blog");
+        mAuth=FirebaseAuth.getInstance();
 
         mPost_key=getIntent().getExtras().getString("blog_id");
 
         mBlogSingleDesc=(TextView)findViewById(R.id.singleBlogDesc);
         mBlogSingleTitle=(TextView)findViewById(R.id.singleBlogTitle);
         mBlogSingleImage=(ImageView)findViewById(R.id.singleBlogImage);
+        mBlogSingleRemoveBtn=(Button)findViewById(R.id.mSingleRemoveBtn);
 
-       // Toast.makeText(BlogSingleActivity.this, post_key, Toast.LENGTH_SHORT).show();
 
         mDatabase.child(mPost_key).addValueEventListener(new ValueEventListener() {
             @Override
@@ -53,12 +62,29 @@ public class BlogSingleActivity extends AppCompatActivity {
 
                 Picasso.with(BlogSingleActivity.this).load(post_image).into(mBlogSingleImage);
 
+                if (mAuth.getCurrentUser().getUid().equals(post_uid)){
+
+                    mBlogSingleRemoveBtn.setVisibility(View.VISIBLE);
+
+                }
+
 
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
+
+        mBlogSingleRemoveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mDatabase.child(mPost_key).removeValue();
+
+                Intent mainIntent=new Intent(BlogSingleActivity.this,MainActivity.class);
+                startActivity(mainIntent);
             }
         });
 
